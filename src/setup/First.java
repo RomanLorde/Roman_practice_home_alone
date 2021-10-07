ca245da3fcc77f671ef95901ad024a11fc7ce8ba
roma3 <roma38lorde@gmail.com>
2021-10-07 12:17:38 -0400

    updated java file

--- src/setup/First.java
+++ src/setup/First.java
@@ -5,5 +5,11 @@ public class First {
 
         System.out.println("hello world");
 
+        //random tet
+
+        System.out.println("Update");
+        int a  = 0;
+        System.out.println(a);
+
     }
 }
