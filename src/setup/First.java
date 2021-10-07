b6755f40503380a29ac122cf6b3e6f6ac234b386
roma3 <roma38lorde@gmail.com>
2021-10-07 14:24:13 -0400

    Revert "delet all file"
    This reverts commit ecf09760a9f2e2b75d27f200a57020e2eb6b2de4.

---
+++ src/setup/First.java
@@ -0,0 +1,15 @@
+package setup;
+
+public class First {
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+        //random tet
+
+        System.out.println("Update");
+        int a  = 0;
+        System.out.println(a);
+
+    }
+}
